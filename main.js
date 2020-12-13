d65b48695c7bf48614b7521ae83c5ad8074b45cf
Chamaka Rajapaksha <chamakarajapaksha@gmail.com>
2020-12-13 21:19:35 +0530

    c

--- main.js
+++ main.js
@@ -13,19 +13,33 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const loader = new THREE.FontLoader();
+// const loader = new THREE.FontLoader();
 
-loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+// loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
 
-	const geometry = new THREE.TextGeometry( 'Sample three.js!', {
-		font: font,
-		size: 80,
-		height: 5,
-		curveSegments: 12,
-		bevelEnabled: true,
-		bevelThickness: 10,
-		bevelSize: 8,
-		bevelOffset: 0,
-		bevelSegments: 5
-	} );
-} );
+// 	const geometry = new THREE.TextGeometry( 'Sample three.js!', {
+// 		font: font,
+// 		size: 80,
+// 		height: 5,
+// 		curveSegments: 12,
+// 		bevelEnabled: true,
+// 		bevelThickness: 10,
+// 		bevelSize: 8,
+// 		bevelOffset: 0,
+// 		bevelSegments: 5
+// 	} );
+// } );
+
+const geometry = new THREE.BoxGeometry(2,2,2);
+const material = new THREE.MeshBasicMaterial({color : 0x00ff00});
+const cube = new THREE.Mesh(geometry, material);
+
+camera.position.z = 5;
+
+scene.add(cube);
+
+function animate(){
+	requestAnimationFrame(animate);
+
+	renderer.render(scene, camera);
+}
